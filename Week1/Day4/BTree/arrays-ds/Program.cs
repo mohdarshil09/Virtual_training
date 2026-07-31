4743dd5ad5d3514e494fbd4b6b318110147d6775
Mohd Arshil Azim <119030009+mohdarshil09@users.noreply.github.com>
2026-07-31 17:07:14 +0530

    Add files via upload
    Hackerrank Solution

---
+++ Week1/Day4/BTree/arrays-ds/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace arrays_ds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+                Console.Write(arr[i]);
+
+                if (i > 0)
+                    Console.Write(" ");
+            }
+        }
+    }
+}
