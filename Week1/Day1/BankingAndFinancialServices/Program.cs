1bc3fb62aeeac8ffe8d518d370b4922a0d3b36b3
Mohd Arshil Azim <119030009+mohdarshil09@users.noreply.github.com>
2026-07-31 15:14:41 +0530

    Add files via upload

---
+++ Week1/Day1/BankingAndFinancialServices/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace BankingAndFinancialServices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Transaction[] transactions =
+            {
+                new Transaction { AccountId="A12", Amount=5000, Time="10:05", Merchant="Amazon" },
+                 new Transaction { AccountId="A14", Amount=20000, Time="10:16", Merchant="Flipkart" },
+                new Transaction { AccountId="A12", Amount=7000, Time="10:34", Merchant="Amazon" },
+                new Transaction { AccountId="A11", Amount=1500, Time="11:04", Merchant="Swiggy" },
+                new Transaction { AccountId="A11", Amount=30000, Time="11:38", Merchant="Amazon" },
+                 new Transaction { AccountId="A12", Amount=16000, Time="09:20", Merchant="Flipkart" }
+
+            };
+
+            Console.WriteLine("Large Transactions");
+            LargeTransactions(transactions);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Repeated Account Transactions");
+            RepeatedTransactions(transactions);
+        }
+
+        // Large amount check if amount is greater than 10000
+        static void LargeTransactions(Transaction[] transactions)
+        {
+           for (int i = 0; i < transactions.Length; i++)
+            {
+             if (transactions[i].Amount > 10000)
+                {
+                    Console.WriteLine(
+                        transactions[i].AccountId + "  " +
+                        transactions[i].Amount + "  " +
+                        transactions[i].Merchant);
+                }
+            }
+        }
+
+        // Same account repeated 
+        static void RepeatedTransactions(Transaction[] transactions)
+        {
+            for (int i = 0; i < transactions.Length; i++)
+            {
+               for (int j = i + 1; j < transactions.Length; j++)
+                {
+                    if (transactions[i].AccountId == transactions[j].AccountId)
+                    {
+                        Console.WriteLine(transactions[i].AccountId + " has multiple transactions.");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Week1/Day1/BankingAndFinancialServices/Transaction.cs
@@ -0,0 +1,10 @@
+﻿namespace BankingAndFinancialServices
+{
+    class Transaction
+    {
+        public string AccountId;
+        public double Amount;
+        public string Time;
+        public string Merchant;
+    }
+}
