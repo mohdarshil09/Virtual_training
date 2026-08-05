5a9d4eaddcd22667ce5ff566dd909a42d50a2a48
Mohd Arshil Azim <119030009+mohdarshil09@users.noreply.github.com>
2026-08-05 11:57:18 +0530

    Add files via upload

---
+++ Week2/Day6/BubbleSort/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    internal class Program
+    {
+        static void BubbleSort(int[] arr)
+        {
+            int n = arr.Length;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                bool swapped = false;
+
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                        swapped = true;
+                    }
+                }
+
+                if (!swapped)
+                    break;
+            }
+        }
+
+        
+
+        static void Main(string[] args)
+        {
+            int[] arr = { 29, 4, 71, 15, 92, 8, 46, 33, 60, 1 };
+
+            Console.WriteLine("Before: [" + string.Join(", ", arr) + "]");
+
+            BubbleSort(arr);
+
+            Console.WriteLine("After: [" + string.Join(", ", arr) + "]");
+
+        }
+    }
+}
---
+++ Week2/Day6/InsertionSort/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace InsertionSort
+{
+    internal class Program
+    {
+        static void InsertionSort(int[] arr)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int key = arr[i];
+                int j = i - 1;
+
+                while (j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+
+                arr[j + 1] = key;
+            }
+        }
+
+        static bool IsSorted(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] > arr[i + 1])
+                    return false;
+            }
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr = { 29, 4, 71, 15, 92, 8, 46, 33, 60, 1 };
+
+            Console.WriteLine("Before: [" + string.Join(", ", arr) + "]");
+
+            // Create Stopwatch
+            Stopwatch stopwatch = new Stopwatch();
+
+            // Start timing
+            stopwatch.Start();
+
+            InsertionSort(arr);
+
+            // Stop timing
+            stopwatch.Stop();
+
+            Console.WriteLine("After: [" + string.Join(", ", arr) + "]");
+
+            Console.WriteLine("Valid Sort: " + IsSorted(arr));
+
+            Console.WriteLine($"\nExecution Time: {stopwatch.Elapsed.TotalMilliseconds:F6} ms");
+        }
+    }
+}
