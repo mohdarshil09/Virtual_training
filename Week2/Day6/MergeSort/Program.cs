66ba609129d734227a3a076572ee3c559747bd73
Mohd Arshil Azim <119030009+mohdarshil09@users.noreply.github.com>
2026-08-05 11:58:44 +0530

    Add files via upload

---
+++ Week2/Day6/MergeSort/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace MergeSort
+{
+    internal class Program
+    {
+        static void Merge(int[] arr, int left, int mid, int right)
+        {
+            int n1 = mid - left + 1;
+            int n2 = right - mid;
+
+            int[] L = new int[n1];
+            int[] R = new int[n2];
+
+            for (int i = 0; i < n1; i++)
+                L[i] = arr[left + i];
+
+            for (int j = 0; j < n2; j++)
+                R[j] = arr[mid + 1 + j];
+
+            int x = 0, y = 0, k = left;
+
+            while (x < n1 && y < n2)
+            {
+                if (L[x] <= R[y])
+                {
+                    arr[k] = L[x];
+                    x++;
+                }
+                else
+                {
+                    arr[k] = R[y];
+                    y++;
+                }
+                k++;
+            }
+
+            while (x < n1)
+            {
+                arr[k] = L[x];
+                x++;
+                k++;
+            }
+
+            while (y < n2)
+            {
+                arr[k] = R[y];
+                y++;
+                k++;
+            }
+        }
+
+        static void MergeSort(int[] arr, int left, int right)
+        {
+            if (left < right)
+            {
+                int mid = (left + right) / 2;
+
+                MergeSort(arr, left, mid);
+                MergeSort(arr, mid + 1, right);
+
+                Merge(arr, left, mid, right);
+            }
+        }
+
+        static bool IsSorted(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] > arr[i + 1])
+                    return false;
+            }
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr = { 29, 4, 71, 15, 92, 8, 46, 33, 60, 1 };
+
+            Console.WriteLine("Before: [" + string.Join(", ", arr) + "]");
+
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+
+            MergeSort(arr, 0, arr.Length - 1);
+
+            stopwatch.Stop();
+
+            Console.WriteLine("After: [" + string.Join(", ", arr) + "]");
+
+            Console.WriteLine("Valid Sort: " + IsSorted(arr));
+
+            Console.WriteLine($"\nExecution Time: {stopwatch.Elapsed.TotalMilliseconds:F6} ms");
+        }
+    }
+}
---
+++ Week2/Day6/SelectionSort/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace SelectionSort
+{
+    internal class Program
+    {
+        static void SelectionSort(int[] arr)
+        {
+            int n = arr.Length;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                int minIndex = i;
+
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (arr[j] < arr[minIndex])
+                        minIndex = j;
+                }
+
+                int temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr = { 29, 4, 71, 15, 92, 8, 46, 33, 60, 1 };
+
+            Console.WriteLine("Before: [" + string.Join(", ", arr) + "]");
+
+            // Create Stopwatch
+            Stopwatch stopwatch = new Stopwatch();
+
+            // Start timing
+            stopwatch.Start();
+
+            SelectionSort(arr);
+
+            // Stop timing
+            stopwatch.Stop();
+
+            Console.WriteLine("After: [" + string.Join(", ", arr) + "]");
+
+            Console.WriteLine($"\nExecution Time: {stopwatch.Elapsed.TotalMilliseconds:F6} ms");
+        }
+    }
+}
