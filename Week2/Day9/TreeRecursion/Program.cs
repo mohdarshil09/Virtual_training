f397e6f9a4ed62d7a21b6af7dca18ca019ad70eb
Mohd Arshil Azim <119030009+mohdarshil09@users.noreply.github.com>
2026-08-07 12:17:08 +0530

    Implement CountPaths method for grid traversal
    Added a method to count paths in a grid using tree recursion.

--- Week2/Day9/TreeRecursion/Program.cs
+++ Week2/Day9/TreeRecursion/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace TreeRecursion
+using System;
+
+namespace TreeRecursion
 {
     internal class Program
     {
+        static int CountPaths(int rows, int cols)
+        {
+            // Base Case
+            if (rows == 1 || cols == 1)
+                return 1;
+
+            // Tree Recursion
+            return CountPaths(rows - 1, cols) + CountPaths(rows, cols - 1);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter number of rows: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter number of columns: ");
+            int cols = Convert.ToInt32(Console.ReadLine());
+
+            int paths = CountPaths(rows, cols);
+
+            Console.WriteLine($"\nNumber of Paths = {paths}");
+
+            Console.ReadKey();
         }
     }
 }
