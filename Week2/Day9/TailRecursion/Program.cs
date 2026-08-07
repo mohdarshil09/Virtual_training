e713a2676b91751c5e7ea946b0f8053d00f6e06c
Mohd Arshil Azim <119030009+mohdarshil09@users.noreply.github.com>
2026-08-07 12:16:40 +0530

    Implement tail recursion to print numbers
    Added a tail recursion method to print numbers in reverse order.

--- Week2/Day9/TailRecursion/Program.cs
+++ Week2/Day9/TailRecursion/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace TailRecursion
+﻿using System;
+
+namespace TailRecursion
 {
     internal class Program
     {
+        static void Tail(int n)
+        {
+            // Base Case
+            if (n == 0)
+                return;
+
+            // Work before recursion
+            Console.Write(n + " ");
+
+            // Recursive Call
+            Tail(n - 1);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nTail Recursion Output:");
+
+            Tail(number);
+
+            Console.ReadKey();
         }
     }
 }
