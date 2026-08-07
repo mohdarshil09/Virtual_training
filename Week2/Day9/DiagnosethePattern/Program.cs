b865e9a2a9f6120d4ff3fb57aca9a92e1421af0a
Mohd Arshil Azim <119030009+mohdarshil09@users.noreply.github.com>
2026-08-07 12:10:35 +0530

    Add files via upload

---
+++ Week2/Day9/DiagnosethePattern/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace DiagnosethePattern
+{
+    internal class Program
+    {
+        // Head Recursion
+        static void Head(int n)
+        {
+            if (n == 0)
+                return;
+
+            Head(n - 1);
+            Console.Write(n + " ");
+        }
+
+        // Tail Recursion
+        static void Tail(int n)
+        {
+            if (n == 0)
+                return;
+
+            Console.Write(n + " ");
+            Tail(n - 1);
+        }
+
+        // Tree Recursion
+        static int Tree(int n)
+        {
+            if (n <= 1)
+                return 1;
+
+            return Tree(n - 1) + Tree(n - 2);
+        }
+
+        // Indirect Recursion
+        static void Even(int n)
+        {
+            if (n == 0)
+            {
+                Console.WriteLine("Even");
+                return;
+            }
+
+            Odd(n - 1);
+        }
+
+        static void Odd(int n)
+        {
+            if (n == 0)
+            {
+                Console.WriteLine("Odd");
+                return;
+            }
+
+            Even(n - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Head Recursion:");
+            Head(5);
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Tail Recursion:");
+            Tail(5);
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Tree Recursion:");
+            Console.WriteLine(Tree(5));
+
+            Console.WriteLine();
+
+            Console.WriteLine("Indirect Recursion:");
+            Even(5);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Week2/Day9/HeadRecursion/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HeadRecursion
+{
+    internal class Program
+    {
+        static void Head(int n)
+        {
+            // Base Case
+            if (n == 0)
+                return;
+
+            // Recursive Call
+            Head(n - 1);
+
+            // Work after recursion
+            Console.Write(n + " ");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nHead Recursion Output:");
+
+            Head(number);
+
+            Console.ReadKey();
+        }
+    }
+}
