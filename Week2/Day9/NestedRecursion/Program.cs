fb3655eb844559787941f1e85eb25ab8a28387ae
Mohd Arshil Azim <119030009+mohdarshil09@users.noreply.github.com>
2026-08-07 12:16:03 +0530

    Implement nested recursion in Program.cs
    Added a nested recursive function to process input numbers.

--- Week2/Day9/NestedRecursion/Program.cs
+++ Week2/Day9/NestedRecursion/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace NestedRecursion
+using System;
+
+namespace NestedRecursion
 {
     internal class Program
     {
+        static int Fun(int n)
+        {
+            // Base Case
+            if (n > 100)
+                return n - 10;
+
+            // Nested Recursive Call
+            return Fun(Fun(n + 11));
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int result = Fun(number);
+
+            Console.WriteLine("\nResult = " + result);
+
+            Console.ReadKey();
         }
     }
 }
