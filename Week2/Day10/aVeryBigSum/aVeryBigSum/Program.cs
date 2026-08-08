a99e91bb5a74cf97cdbbbe018e8b5bd1948afe83
Mohd Arshil Azim <119030009+mohdarshil09@users.noreply.github.com>
2026-08-08 10:36:03 +0530

    Add files via upload

---
+++ Week2/Day10/aVeryBigSum/aVeryBigSum/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Result
+{
+    public static long aVeryBigSum(List<long> ar)
+    {
+        long sum = 0;
+
+        foreach (long num in ar)
+        {
+            sum += num;
+        }
+
+        return sum;
+    }
+}
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int arCount = int.Parse(Console.ReadLine());
+
+        List<long> ar = new List<long>();
+
+        string[] input = Console.ReadLine().Split(' ');
+
+        for (int i = 0; i < arCount; i++)
+        {
+            ar.Add(long.Parse(input[i]));
+        }
+
+        long result = Result.aVeryBigSum(ar);
+
+        Console.WriteLine(result);
+    }
+}
